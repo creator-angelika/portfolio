82bda292d55b84f8992c9e7ea1ceb8cd21cc235a
Angel <angelikathomas26@gmail.com>
2023-12-09 20:27:07 +0530

    git commmit

--- src/components/interface.jsx
+++ src/components/interface.jsx
@@ -113,6 +113,10 @@ const skills = [
         level: 85,
     },
     {
+        title: "Github/ GIT",
+        level: 80,
+    },
+    {
         title: "Machine Learning",
         level: 80,
     },
